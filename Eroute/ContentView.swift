ab2b4c3c69772196c5d03c8bea38b18e43adb471
Bhavesh <bhavesh.gupta@techtreeit.com>
2021-09-24 20:18:25 +0530

    Initial Commit

---
+++ Eroute/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Eroute
+//
+//  Created by bhavesh on 24/09/21.
+//  Copyright © 2021 Bhavesh. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
