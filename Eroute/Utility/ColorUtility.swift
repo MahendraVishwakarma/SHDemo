9d645e26d2293d8270eb523e76b9d46f5414386b
Mahendra Vishwakarma <mahendra010895@gmail.com>
2021-12-29 01:15:36 +0530

    updted cd

--- Eroute/Utility/ColorUtility.swift
+++
@@ -1,9 +0,0 @@
-//
-//  ColorUtility.swift
-//  Eroute
-//
-//  Created by Bhavesh on 28/12/21.
-//  Copyright © 2021 Bhavesh. All rights reserved.
-//
-
-import Foundation
---
+++ SH/Utility/Error/DataBaseError.swift
@@ -0,0 +1,15 @@
+//
+//  DataBaseError.swift
+//  Eroute
+//
+//  Created by Mahendra on 28/12/21.
+//  Copyright © 2021 Mahendra. All rights reserved.
+//
+
+import Foundation
+
+
+enum DataBaseError: Error {
+    case badExcess
+    case noDataFound(String)
+}
